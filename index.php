c10a32f78093ca655a120a611f60f8d303752337
Marco-Vigilio <tonyvigilio@gmail.com>
2023-07-03 17:13:08 +0200

    aggiugo colore + aggiungo filtri

--- index.php
+++ index.php
@@ -42,6 +42,19 @@
 
     $empty = 0;
 
+    $newArray = [];
+
+   var_dump($_GET["parking"]);
+/*
+    if($_GET["parking"] === on){
+        foreach ($hotels as $key => $value) {
+            var_dump($value);
+        }
+    }
+    else{
+        $newArray = $hotels;
+    }
+*/
 ?>
 
 <html lang="en">
@@ -52,19 +65,35 @@
         <title>Php Hotel</title>
     </head>
     <body>
-        <header>
-            
-        </header>
         <main>
-            <h1 class="center">Php Hotel</h1>
-            <div class="contain">
+        <h1 class="center">Php Hotel</h1>
+        <div class="contain">
+            <form class="flex" action="./index.php" method="get">
+                <label for="park">Parking</label>
+                <input type="checkbox" name="parking" id="parking">
+
+                <label for="stars">Hotel with stars</label>
+                <input type="text" name="stars" id="stars">
+
+                <input type="submit" value="Search">
+            </form>
+        </div>
+        
+            <div class="contain flex">
                 <?php foreach ($hotels as $key => $value) { ?>
                     <div class="item">
                         <h2 class="center"><?php echo $value["name"] ?></h2>
                         <div class="info">
                             <p class="center"><?php echo $value["description"] ?></p>
                             <ul>
-                                <li>parking: <?php echo $value["parking"] ?></li>
+                                <li>parking: 
+                                    <?php if(!empty($value["parking"])){
+                                        echo $value["parking"];
+                                    }else{
+                                        echo $empty;
+                                    }
+                                    ?>
+                                </li>
                                 <li>vote: <?php echo $value["vote"] ?></li>
                                 <li>distance to center: <?php echo $value["distance_to_center"] ?> km</li>
                             </ul>
