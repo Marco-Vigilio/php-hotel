3b445c912357e089e4917f3b8c32312369b72b5f
Marco-Vigilio <tonyvigilio@gmail.com>
2023-07-03 15:30:22 +0200

    visualizzazione degli hotel con le determinate info

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<?php
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Hotel</title>
+</head>
+<body>
+    <h1>Php Hotel</h1>
+    <?php foreach ($hotels as $key => $value) { ?>
+        <div>
+            <p><?php echo $value["name"] ?></p>
+            <p><?php echo $value["description"] ?></p>
+            <p><?php echo $value["parking"] ?></p>
+            <p><?php echo $value["vote"] ?></p>
+            <p><?php echo $value["distance_to_center"] ?></p>
+        </div>
+    <?php } ?>
+</body>
+</html>
